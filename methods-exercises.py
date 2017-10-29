aa713f38af37d8608db1edbdff95c5fafff9020a
Tarik Abdel <31406864+ny-sec@users.noreply.github.com>
2017-10-29 17:51:22 -0400

    Create methods-exercises.py

---
+++ methods-exercises.py
@@ -0,0 +1,10 @@
+# Complete the method by making sure it returns 42. .
+def return_42():
+    return 42
+    # Complete method here
+    pass  # 'pass' just means "do nothing". Make sure to delete this!
+
+# Create a method below, called my_method, that takes two arguments and returns the result of its two arguments multiplied together.
+def my_method(arg1, arg2):
+    return arg1 * arg2
+print(my_method)
